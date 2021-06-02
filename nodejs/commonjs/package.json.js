7f67e39d757fb26d448accd3b1eb6dd57c6b6e56
justin <978336184@qq.com>
2021-06-02 15:38:50 +0800

    nodejs包管理

---
+++ nodejs/commonjs/package.json.js
@@ -0,0 +1,9 @@
+// 包模块
+// 版本	                 变更
+// v14.13.0	            Add support for "exports" patterns.  （支持正则 exports）
+// v14.6.0	            Add package "imports" field.  （添加imports字段）
+// v13.7.0, v12.16.0	Unflag conditional exports.    （取消标记条件导出）
+// v13.6.0, v12.16.0	Unflag self-referencing a package using its name.  （使用包名取消标记自调用包）
+// v12.7.0	            Introduce "exports" package.json field as a more powerful alternative to the classic "main" field.（添加exports字段，扩展main导出入口）
+// v12.0.0	            Add support for ES modules using .js file extension via package.json "type" field.（package.json type=module或者命令行--input-type=module字段支持es 模块）
+
